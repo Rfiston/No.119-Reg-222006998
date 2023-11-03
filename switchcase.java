1d023224fcd8410d4bf10af300803a50a1e370f8
Rfiston <149690999+Rfiston@users.noreply.github.com>
2023-11-03 14:46:11 +0200

    Add files via upload
    java program

---
+++ switchcase.java
@@ -0,0 +1,17 @@
+public class switchcase {
+    public static void main(String[] args) {
+        
+        int amount=50000;
+        switch(amount)
+        {
+case 1 << 49999: System.out.println("you're not allowed to hostel");
+break;
+case 50000: System.out.println("you're allowed to hostel"); 
+break;
+default: System.out.println("invalid amount");
+
+        }
+
+    }
+    
+}
