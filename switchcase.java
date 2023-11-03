8366d5c0296ebe2479f461e544a4883baaff4d41
Rfiston <149690999+Rfiston@users.noreply.github.com>
2023-11-03 15:11:58 +0200

    Update switchcase.java

--- switchcase.java
+++ switchcase.java
@@ -1,17 +1,18 @@
-public class switchcase {
-    public static void main(String[] args) {
-        
+// this program display peaple allowed to hostel
+public class switchcase { // name of class
+    public static void main(String[] args) { //main class
+         //  declaration and initialization variable
         int amount=50000;
         switch(amount)
         {
-case 1 << 49999: System.out.println("you're not allowed to hostel");
+case 1 << 49999: System.out.println("you're not allowed to hostel"); // output
 break;
-case 50000: System.out.println("you're allowed to hostel"); 
+case 50000: System.out.println("you're allowed to hostel"); // output
 break;
-default: System.out.println("invalid amount");
+default: System.out.println("invalid amount"); // output
 
         }
 
     }
     
-}
+} // end of program
