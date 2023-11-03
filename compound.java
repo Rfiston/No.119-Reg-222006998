a01114bf963b7826ee3a189aa7c277d199fe80b3
Rfiston <149690999+Rfiston@users.noreply.github.com>
2023-11-03 15:02:59 +0200

    Update compound.java

--- compound.java
+++ compound.java
@@ -1,9 +1,10 @@
-public class compound { 
-    static double comp(double p, double r, double t, double n){
-        return  p*(Math.pow(1+(r/(n*100)),n*t));
+// this program display amount to be paid after three years
+public class compound { // class name
+    static double comp(double p, double r, double t, double n){ // method name with variables declaration
+        return  p*(Math.pow(1+(r/(n*100)),n*t)); // output to be returned
     }
-    public static void main(String[] args) {
-        compound C= new compound();
-        System.out.println("the amount he will have paid after 3 years period is equal to: " + comp(500000,18,3,12));
+    public static void main(String[] args) { // main class 
+        compound C= new compound(); // object name
+        System.out.println("the amount he will have paid after 3 years period is equal to: " + comp(500000,18,3,12));//output
     }
-}
+} // end of program
