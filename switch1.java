d9a414111a04d9f37422dd8e5031044d6effaba8
Rfiston <149690999+Rfiston@users.noreply.github.com>
2023-11-03 15:15:07 +0200

    Add files via upload
    switch1.java

---
+++ switch1.java
@@ -0,0 +1,33 @@
+// this program display days of a week 
+public class switch1 { // class name
+    public static void main(String[] args) { // main class
+        //  declaration and initialization variable
+        int week=6; 
+        switch (week) {
+            case 1:
+                  System.out.println("Monday");    //output            
+                break;
+            case 2:  System.out.println("Tuesday"); //output
+                break;
+           case 3:  System.out.println("Wensday"); //output
+                break;
+            case 4: System.out.println("Thuesday"); //output
+                break;
+            case 5: System.out.println("Friday"); //output
+                break;
+            case 6: System.out.println("Satarday"); //output
+                break;
+            case 7: System.out.println("Sunday"); //output
+            break;
+
+            default: System.out.println("invalid day"); //output
+                break;
+        }
+    }
+} // end of a program
+
+
+
+
+
+
