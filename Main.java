7459bc4aa067b0b01e69b09b3d0a3a947398f9c9
Rfiston <149690999+Rfiston@users.noreply.github.com>
2023-11-03 14:59:25 +0200

    simple.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
-public class Main {
-    static int interest(int p, int r, int t){
-        return (p*r*t)/100;
+// this program display simple interest
+public class Main { // this is the main class
+    static int interest(int p, int r, int t){ // name of the method
+        return (p*r*t)/100; // output that will be returned
     }
 
-    public static void main(String[] args) {
-        Main I = new Main();
-        System.out.println("simple interest is = " + I.interest(100000, 5, 5));
+    public static void main(String[] args) { // main class
+        Main I = new Main(); // object name
+        System.out.println("simple interest is = " + I.interest(100000, 5, 5)); // out put
     }    
-} 
+} // end of a program 
